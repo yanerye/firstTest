780016886dc03eec0170476730b35e4528447172
脉络 <mailuo@mailuodeMacBook-Pro.local>
2019-06-18 17:03:22 +0800

    第二次提交

---
+++ test/AddViewController.h
@@ -0,0 +1,17 @@
+//
+//  AddViewController.h
+//  test
+//
+//  Created by 脉络 on 2019/6/18.
+//  Copyright © 2019 脉络. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AddViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ test/AddViewController.m
@@ -0,0 +1,32 @@
+//
+//  AddViewController.m
+//  test
+//
+//  Created by 脉络 on 2019/6/18.
+//  Copyright © 2019 脉络. All rights reserved.
+//
+
+#import "AddViewController.h"
+
+@interface AddViewController ()
+
+@end
+
+@implementation AddViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
